dee6ce0ee3c532f9534d68c80d9edfb8207b9ab1
PrasannaTulluri <60947137+PrasannaTulluri@users.noreply.github.com>
2020-02-12 02:21:47 +0000

    Create TestData.js

---
+++ Data/TestData.js
@@ -0,0 +1,22 @@
+module.exports.data=
+{
+    noOfItems_data              : '2',
+    size_data                   : 'M',
+    color_data                  : 'Orange',
+    email_data                  : 'Alexa101g@gmail.com',
+    title_data /*Mr./Mrs*/      : 'Mrs.',
+    firstName_data              : 'Alexa',
+    lastName_data               : 'G',
+    password_data               : 'Password123',
+    date_data /*mm/dd/yyyy*/    : '03/09/1990',
+    company_data                : 'Google',
+    address1_data               : '3 Highway',
+    address2_data               : 'Rose Garden',
+    city_data                   : 'San Jose',
+    state_data                  : 'California',
+    postcode_data               : '12345',
+    country_data                : 'United States',
+    phone_data                  : '9876501234',
+    mobile_data                 : '9876543210',
+    successMsg_data             : 'Your order on My Store is complete.'
+};
